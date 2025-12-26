f4e00b97f87b556280b6aa692026a435ac51428b
Ariel Bereslavsky <ariael2009@gmail.com>
2025-12-27 00:53:48 +0200

    Fix zsh completion: handle missing compdef gracefully
    - Check if compdef is available before calling it
    - Defer completion setup if compinit hasn't run yet
    - Use precmd hook to set up completions after compinit loads

--- completions/slx.zsh
+++ completions/slx.zsh
@@ -1,6 +1,34 @@
 # Zsh completion for slx (SLurm eXtended)
-# Add to your ~/.zshrc:
+# Add to your ~/.zshrc (after compinit):
 #   source ~/.local/share/slx/completions/slx.zsh
+
+# Ensure compdef is available (requires compinit to be loaded first)
+if ! type compdef &>/dev/null; then
+    # If compinit hasn't been run yet, autoload it
+    autoload -Uz compinit
+    # Don't run compinit here - just make sure compdef is available
+    # by setting up the function to be deferred
+    _slx_setup_completions() {
+        compdef _slx slx
+        compdef _slx_submit cs
+        compdef _slx_job_id cl ct ck ci
+        compdef _slx_find cf
+        compdef _slx_project_submit cps
+    }
+    # Register to run after compinit
+    if [[ -z "$_comps" ]]; then
+        # compinit hasn't run yet, hook into precmd to set up later
+        autoload -Uz add-zsh-hook
+        _slx_deferred_setup() {
+            if type compdef &>/dev/null; then
+                _slx_setup_completions
+                add-zsh-hook -d precmd _slx_deferred_setup
+                unfunction _slx_deferred_setup
+            fi
+        }
+        add-zsh-hook precmd _slx_deferred_setup
+    fi
+fi
 
 _slx() {
     local -a commands project_commands
@@ -78,8 +106,6 @@ _slx() {
     esac
 }
 
-compdef _slx slx
-
 # Completion for aliases
 _slx_submit() {
     _files -g '*.sbatch'
@@ -107,8 +133,11 @@ _slx_project_submit() {
     fi
 }
 
-compdef _slx_submit cs
-compdef _slx_job_id cl ct ck ci
-compdef _slx_find cf
-compdef _slx_project_submit cps
-
+# Register completions if compdef is available
+if type compdef &>/dev/null; then
+    compdef _slx slx
+    compdef _slx_submit cs
+    compdef _slx_job_id cl ct ck ci
+    compdef _slx_find cf
+    compdef _slx_project_submit cps
+fi
